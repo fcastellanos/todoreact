fe36e8c1893e62ffd5f622a6b746dd4baf851486
Fernando Castellanos <fcastellanos@bandsintown.com>
2018-08-21 14:22:50 -0700

    First commit

---
+++ src/Task.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {Row, Col} from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
+
+class Task extends Component {
+
+}
+
+export default Task;
