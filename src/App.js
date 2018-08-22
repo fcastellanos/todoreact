dbf3f72352394a9a63e3f4bfbc469c827760d355
Fernando Castellanos <fcastellanos@bandsintown.com>
2018-08-21 17:06:44 -0700

    Adding the tasks to App.js and passing it to TaskList through props

--- src/App.js
+++ src/App.js
@@ -7,6 +7,44 @@ import AddButton from './AddButton.js';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks: [
+        {
+          'time': '12',
+          'period': 'AM',
+          'activity_title': 'Finish Tutorial Series',
+          'activity_description': '#ReactForNewbies'
+        },
+        {
+          'time': '9',
+          'period': 'AM',
+          'activity_title': 'Meeting with Team Leads',
+          'activity_description': 'New Project Kickoff'
+        },
+        {
+          'time': '11',
+          'period': 'AM',
+          'activity_title': 'Call Mom',
+          'activity_description': 'Return her call before she kills me'
+        },
+        {
+          'time': '3',
+          'period': 'PM',
+          'activity_title': 'Fix Wifey\'s website',
+          'activity_description': 'FB Ads Integration not working'
+        },
+        {
+          'time': '6',
+          'period': 'PM',
+          'activity_title': 'Do DB Backups',
+          'activity_description': 'Related to upcoming server migration'
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div style={{padding: '30px 30px'}}>
@@ -14,7 +52,7 @@ class App extends Component {
         <br />
         <Date />
         <br />
-        <TaskList />
+        <TaskList tasks={this.state.tasks} />
         <br />
         <AddButton />
       </div>
